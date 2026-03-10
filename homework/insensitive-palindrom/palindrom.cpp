0e726b48cd7f93bb0d51e6f70704bd7202216e7b
Sebastian Sosnowski <mefiu672@gmail.com>
2026-03-10 21:09:29 +0100

    Structure that is building and compiling

---
+++ homework/insensitive-palindrom/palindrom.cpp
@@ -0,0 +1,11 @@
+// Napisz program, który będzie sprawdzał, czy podany ciąg wyrazów jest palindromem. Program powinien ignorować znaki specjalne np. ,.?()[] (i nie tylko). Powinien również ignorować wielkość liter oraz białe znaki jak spacja czy znak nowej linii.
+
+// Wywnioskuj po testach jak nazwać funkcję. Stwórz własne pliki źródłowe i nagłówkowe i dodaj je do CMakeLists.txt, aby się budowały. Szczegóły w README.md do tego zadania.
+
+// Gotowe zadanie zgłoś na gałąź coders-school:insensitive-palindrom.
+
+#include "palindrom.hpp"
+
+bool is_palindrome(std::string palindrom) {
+    return true;
+}
