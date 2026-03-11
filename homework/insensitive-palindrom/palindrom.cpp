c2d69879f8bc97e68f699791e7cab1f905e95503
Sebastian Sosnowski <mefiu672@gmail.com>
2026-03-11 20:04:58 +0100

    Refactor using STL
    Used filter, transform and equal. Time complexity is still O(N), but in practise this will be slower than version with normal while loop.

--- homework/insensitive-palindrom/palindrom.cpp
+++ homework/insensitive-palindrom/palindrom.cpp
@@ -7,27 +7,33 @@ static bool my_isalnum(char ch) {
     return std::isalnum(static_cast<unsigned char>(ch));
 }
 
+// More elegant with STL, but slower, bcs time complexity  O(N)
 bool is_palindrome(std::string word) {
-    auto left = word.begin();
-    auto right = std::prev(word.end());
-    std::cout << *left << " " << *right << "\n";
-    while (left < right) {
-        if (!my_isalnum(*left)) {
-            left++;
-            continue;
-        }
-        if (!my_isalnum(*right)) {
-            right--;
-            continue;
-        }
-        auto l = my_tolower(*left);
-        auto r = my_tolower(*right);
-
-        if (l != r) {
-            return false;
-        }
-        left++;
-        right--;
-    }
-    return true;
+    auto normalized = word | std::views::filter(my_isalnum) | std::views::transform(my_tolower);
+    return std::ranges::equal(normalized, normalized | std::views::reverse);
 }
+//  with while loop, time complexity is O(N/2)
+// bool is_palindrome(std::string word) {
+//     auto left = word.begin();
+//     auto right = std::prev(word.end());
+//     std::cout << *left << " " << *right << "\n";
+//     while (left < right) {
+//         if (!my_isalnum(*left)) {
+//             left++;
+//             continue;
+//         }
+//         if (!my_isalnum(*right)) {
+//             right--;
+//             continue;
+//         }
+//         auto l = my_tolower(*left);
+//         auto r = my_tolower(*right);
+
+//         if (l != r) {
+//             return false;
+//         }
+//         left++;
+//         right--;
+//     }
+//     return true;
+// }
--- homework/insensitive-palindrom/palindrom.hpp
+++ homework/insensitive-palindrom/palindrom.hpp
@@ -1,7 +1,9 @@
 #pragma once
+#include <algorithm>
 #include <cctype>
 #include <ios>
 #include <iostream>
+#include <ranges>
 #include <string>
 
 bool is_palindrome(std::string word);
